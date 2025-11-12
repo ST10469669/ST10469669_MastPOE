8540d6036cf3b535209fce92d82e5c32852d1b09
st10469669 <st10469669@vcconnect.edu.za>
2025-11-12 12:10:20 +0200

    made changaes on the menu screen

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -12,6 +12,7 @@ export default function Welcome({ navigation }: { navigation: any }) {
       source={require("../../assets/chef.webp")}
       style={styles.background}
     >
+      
       <View style={styles.container}>
         <Text style={styles.title}>Welcome to Chef Christoffel's App</Text>
         <Text style={styles.subtitle}>Let's order some delicious meals!</Text>
